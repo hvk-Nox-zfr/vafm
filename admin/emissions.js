8081344e84c8814cc56499a1919634d6420bbf54
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-01-24 18:04:05 +0100

    Update emissions.js

--- admin/emissions.js
+++ admin/emissions.js
@@ -1,6 +1,6 @@
 // Initialisation Supabase
-const supabaseUrl = "https://xxxxx.supabase.co"; // ton URL
-const supabaseKey = "public-anon-key"; // ta clé
+const supabaseUrl = "https://blronpowdhaumjudtgvn.supabase.co"; // ton URL
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscm9ucG93ZGhhdW1qdWR0Z3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODU4MDAsImV4cCI6MjA4NDU2MTgwMH0.ThzU_Eqgwy0Qx2vTO381R0HHvV1jfhsAZFxY-Aw4hXI"; // ta clé
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 // -------------------------
