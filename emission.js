eaca3bc95b47403c7502e8a2b4b1d54d92d0185b
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-01-24 18:42:14 +0100

    Update emission.js

--- emission.js
+++ emission.js
@@ -1,3 +1,5 @@
+console.log("emission.js chargé");
+
 // Connexion Supabase
 const supabaseUrl = "https://blronpowdhaumjudtgvn.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscm9ucG93ZGhhdW1qdWR0Z3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODU4MDAsImV4cCI6MjA4NDU2MTgwMH0.ThzU_Eqgwy0Qx2vTO381R0HHvV1jfhsAZFxY-Aw4hXI";
