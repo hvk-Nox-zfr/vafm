91b2323d72768cdd7e30a05c3969e9d62fb40fa2
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-03-25 13:53:19 +0100

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -5,10 +5,7 @@ import { loadEmissions, setupEmissionForm } from "./emissions.js";
    SUPABASE CLIENT
 ============================================================ */
 
-const supabase = window.supabase.createClient(
-    "https://blronpowdhaumjudtgvn.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscm9ucG93ZGhhdW1qdWR0Z3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODU4MDAsImV4cCI6MjA4NDU2MTgwMH0.ThzU_Eqgwy0Qx2vTO381R0HHvV1jfhsAZFxY-Aw4hXI"
-);
+const supabase = await window.__supabaseReady;
 
 document.addEventListener("DOMContentLoaded", async () => {
 
