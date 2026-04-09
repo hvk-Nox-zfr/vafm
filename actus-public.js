7109460e54dfccf2beb13629e394610eae6d6dd5
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-04-09 20:22:34 +0200

    Update actus-public.js

--- actus-public.js
+++ actus-public.js
@@ -1,6 +1,5 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-const supabase = createClient(
+// Supabase global (UMD)
+const supabase = window.supabase.createClient(
   "https://blronpowdhaumjudtgvn.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscm9ucG93ZGhhdW1qdWR0Z3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODU4MDAsImV4cCI6MjA4NDU2MTgwMH0.ThzU_Eqgwy0Qx2vTO381R0HHvV1jfhsAZFxY-Aw4hXI"
 );
@@ -71,7 +70,6 @@ function creerCarteActu(actu) {
   const text = document.createElement("div");
   text.className = "actu-extrait";
 
-  // Nettoyage du texte pour l'extrait
   let propre = actu.texte
     .replace(/<h2[\s\S]*?<\/h2>/gi, "")
     .replace(/class="floating-text"/g, "")
