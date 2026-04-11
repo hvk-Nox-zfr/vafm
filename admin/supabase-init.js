bd1a7f30af1479870173b9c41720e431b3ee26d3
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-04-11 14:09:50 +0200

    Update supabase-init.js

--- admin/supabase-init.js
+++ admin/supabase-init.js
@@ -4,11 +4,12 @@ const SUPABASE_URL = "https://blronpowdhaumjudtgvn.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscm9ucG93ZGhhdW1qdWR0Z3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODU4MDAsImV4cCI6MjA4NDU2MTgwMH0.ThzU_Eqgwy0Qx2vTO381R0HHvV1jfhsAZFxY-Aw4hXI";
 
 // Création du client Supabase global (UMD)
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// Exposer proprement
-window.supabase = supabase;
-window.__supabaseReady = Promise.resolve(supabase);
+// Exposer proprement SANS ÉCRASER window.supabase
+window.__supabaseClient = supabaseClient;
+window.__supabaseReady = Promise.resolve(supabaseClient);
 
 console.log("supabase-init.js : client UMD initialisé");
-export { supabase };
+
+export { supabaseClient as supabase };
