a7c80470d68732060dc5715e1fc032f4eb067461
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-04-11 14:48:49 +0200

    Update page.js

--- page.js
+++ page.js
@@ -1,8 +1,5 @@
-// Supabase global (UMD)
-const supabase = window.supabase.createClient(
-  "https://blronpowdhaumjudtgvn.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscm9ucG93ZGhhdW1qdWR0Z3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODU4MDAsImV4cCI6MjA4NDU2MTgwMH0.ThzU_Eqgwy0Qx2vTO381R0HHvV1jfhsAZFxY-Aw4hXI"
-);
+// Connexion Supabase (client global unique)
+const supabase = window.__supabaseClient;
 
 const params = new URLSearchParams(window.location.search);
 const idParam = params.get("id");
@@ -68,3 +65,4 @@ async function chargerActu() {
 }
 
 document.addEventListener("DOMContentLoaded", chargerActu);
+
