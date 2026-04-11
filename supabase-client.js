5cc81f9d4970b319d26a14479e111c67bd053983
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-04-11 14:27:39 +0200

    Create supabase-client.js

---
+++ supabase-client.js
@@ -0,0 +1,5 @@
+// supabase-client.js
+const SUPABASE_URL = "https://blronpowdhaumjudtgvn.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+
+window.__supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
