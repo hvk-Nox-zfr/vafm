93c72829c0836ed7244da3bbcf49f580d78ed721
hvk-Nox-zfr <hugoboss.star59@gmail.com>
2026-04-11 14:55:01 +0200

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,5 +1,5 @@
 // supabase-client.js
 const SUPABASE_URL = "https://blronpowdhaumjudtgvn.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJscm9ucG93ZGhhdW1qdWR0Z3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODU4MDAsImV4cCI6MjA4NDU2MTgwMH0.ThzU_Eqgwy0Qx2vTO381R0HHvV1jfhsAZFxY-Aw4hXI";
 
 window.__supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
